d4e5971279c4da3fc981e95fa5ad558a5f50169d
Maximilian Weiss <maximilianw@google.com>
2025-02-02 22:53:57 -0500

    Adds firebase client and firestore persitance operations.

--- terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = var.backend_bucket_name
-    prefix = "genmedia-studio"
-  }
-}
--- terraform/database.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "images" {
-  name          = format("%s-%s", var.project_id, "images")
-  location      = var.region
-  project       = var.project_id
-
-  uniform_bucket_level_access = true
-  public_access_prevention = "inherited"
-}
