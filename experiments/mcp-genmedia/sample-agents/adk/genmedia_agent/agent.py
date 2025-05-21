6075fd1ea5018a66a46cfd181570b631444f629f
GH Chinoy <ghchinoy@google.com>
2025-05-20 18:09:59 -0600

    feat(mcp): sample agents ADK updated to 1.0 syntax

--- experiments/mcp-genmedia/sample-agents/adk/genmedia_agent/agent.py
+++ experiments/mcp-genmedia/sample-agents/adk/genmedia_agent/agent.py
@@ -11,71 +11,66 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 import os
-from contextlib import AsyncExitStack
+from google.adk.agents import LlmAgent
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
 
 from dotenv import load_dotenv
-from google.adk.agents.llm_agent import LlmAgent
-from google.adk.tools.mcp_tool.mcp_toolset import (
-    MCPToolset,
-    SseServerParams,
-    StdioServerParameters,
-)
+
 
 load_dotenv()
 
 project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
+# MCP Client (STDIO)
+# assumes you've installed the MCP server on your path
+veo = MCPToolset(
+    connection_params=StdioServerParameters(
+        command="mcp-veo-go", 
+        env=dict(os.environ, PROJECT_ID=project_id),
+    ),
+)
 
-# ADK with MCP sample
-async def create_agent():
-    """Gets tools from MCP Server."""
-    common_exit_stack = AsyncExitStack()
-     
-    # MCP Client (STDIO)
-    # assumes you've installed the MCP server on your path
-    veo, _ = await MCPToolset.from_server(
-        connection_params=StdioServerParameters(
-            command="mcp-veo-go", 
-            env=dict(os.environ, PROJECT_ID=project_id),
-        ),
-        async_exit_stack=common_exit_stack,
-    )
+chirp3 = MCPToolset(
+    connection_params=StdioServerParameters(
+        command="mcp-chirp3-go",
+        env=dict(os.environ, PROJECT_ID=project_id),
+    ),
+)
 
-    # MCP Client (STDIO)
-    # assumes you've installed the MCP server on your path
-    chirp3, _ = await MCPToolset.from_server(
-        connection_params=StdioServerParameters(
-            command="mcp-chirp3-go",
-            env=dict(os.environ, PROJECT_ID=project_id),
-        ),
-        async_exit_stack=common_exit_stack,
-    )
+imagen = MCPToolset(
+    connection_params=StdioServerParameters(
+        command="mcp-imagen-go",
+        env=dict(os.environ, PROJECT_ID=project_id),
+    ),
+)
 
-    # MCP Client (SSE)
-    # assumes you've started the MCP server separately
-    # e.g. mcp-imagen-go --transport sse
-    remote_imagen, _ = await MCPToolset.from_server(
-        connection_params=SseServerParams(url="http://localhost:8080/sse"),
-        async_exit_stack=common_exit_stack,
-    )
+# MCP Client (SSE)
+# assumes you've started the MCP server separately
+# e.g. mcp-imagen-go --transport sse
+# from google.adk.tools.mcp_tool.mcp_toolset import SseServerParams
+# remote_imagen, _ = MCPToolset(
+#     connection_params=SseServerParams(url="http://localhost:8080/sse"),
+# )
 
-    agent = LlmAgent(
-        model="gemini-2.0-flash",
-        name="genmedia_agent",
-        instruction=(
-            """You're a creative assistant that can help users with creating audio, images, and video via your generative media tools.
-            Feel free to be helpful in your suggestions, based on the information you know or can retrieve from your tools.
-            If you're asked to translate into other languages, please do.
-            """
-        ),
-        tools=[
-            *remote_imagen,
-            *chirp3,
-            *veo,
-        ],
-    )
-    return agent, common_exit_stack
+avtool = MCPToolset(
+    connection_params=StdioServerParameters(
+        command="mcp-avtool-go",
+        env=dict(os.environ, PROJECT_ID=project_id),
+    ),
+)
 
 
-root_agent = create_agent()
+root_agent = LlmAgent(
+    model='gemini-2.0-flash',
+    name='genmedia_agent',
+        instruction="""You're a creative assistant that can help users with creating audio, images, and video via your generative media tools. You also have the ability to composit these using your available tools.
+        Feel free to be helpful in your suggestions, based on the information you know or can retrieve from your tools.
+        If you're asked to translate into other languages, please do.
+        """,
+    tools=[
+       imagen, chirp3, veo, avtool,
+    ],
+)
