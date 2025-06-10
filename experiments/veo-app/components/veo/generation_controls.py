98abc2b75cd932cdb13b924ba1c894bbec052602
Hussain Chinoy <ghchinoy@google.com>
2025-06-10 14:10:00 -0500

    feat(veo): refactor for ease of use

---
+++ experiments/veo-app/components/veo/generation_controls.py
@@ -0,0 +1,90 @@
+import mesop as me
+
+from state.veo_state import PageState
+
+
+@me.component
+def generation_controls():
+    """Video generation controls"""
+    state = me.state(PageState)
+    with me.box(style=me.Style(display="flex", flex_basis="row", gap=5)):
+        me.select(
+            label="aspect",
+            appearance="outline",
+            options=[
+                me.SelectOption(label="16:9 widescreen", value="16:9"),
+                me.SelectOption(label="9:16 portrait", value="9:16"),
+            ],
+            value=state.aspect_ratio,
+            on_selection_change=on_selection_change_aspect,
+            disabled=True if state.veo_model == "3.0" else False,  # 3.0 only does 16:9
+        )
+        me.select(
+            label="length",
+            options=[
+                me.SelectOption(label="5 seconds", value="5"),
+                me.SelectOption(label="6 seconds", value="6"),
+                me.SelectOption(label="7 seconds", value="7"),
+                me.SelectOption(label="8 seconds", value="8"),
+            ],
+            appearance="outline",
+            style=me.Style(),
+            value=f"{state.video_length}",
+            on_selection_change=on_selection_change_length,
+            disabled=True
+            if state.veo_model == "3.0"
+            else False,  # 3.0 only does 8 seconds
+        )
+        me.checkbox(
+            label="auto-enhance prompt",
+            checked=state.auto_enhance_prompt,
+            on_change=on_change_auto_enhance_prompt,
+            disabled=True
+            if state.veo_model == "3.0"
+            else False,  # 3.0 no enhance prompt
+        )
+        me.select(
+            label="model",
+            options=[
+                me.SelectOption(label="Veo 2.0", value="2.0"),
+                me.SelectOption(label="Veo 3.0", value="3.0"),
+            ],
+            appearance="outline",
+            style=me.Style(),
+            value=state.veo_model,
+            on_selection_change=on_selection_change_model,
+        )
+
+
+def on_selection_change_length(e: me.SelectSelectionChangeEvent):
+    """Adjust the video duration length in seconds based on user event"""
+    state = me.state(PageState)
+    state.video_length = int(e.value)
+
+
+def on_selection_change_aspect(e: me.SelectSelectionChangeEvent):
+    """Adjust aspect ratio based on user event."""
+    state = me.state(PageState)
+    state.aspect_ratio = e.value
+
+
+def on_selection_change_model(e: me.SelectSelectionChangeEvent):
+    """Adjust model based on user event."""
+    state = me.state(PageState)
+    state.veo_model = e.value
+    # reset to veo 3 settings
+    if state.veo_model == "3.0":
+        # aspect = 16x9 only
+        # length = 8 seconds
+        # t2v only
+        # no auto enhance
+        state.aspect_ratio = "16:9"
+        state.video_length = 8
+        state.veo_mode = "t2v"
+        state.auto_enhance_prompt = False
+
+
+def on_change_auto_enhance_prompt(e: me.CheckboxChangeEvent):
+    """Toggle auto-enhance prompt"""
+    state = me.state(PageState)
+    state.auto_enhance_prompt = e.checked
---
+++ experiments/veo-app/components/veo/video_display.py
@@ -0,0 +1,74 @@
+import mesop as me
+
+from state.veo_state import PageState
+
+
+@me.component
+def video_display():
+    """Display the generated video"""
+    state = me.state(PageState)
+    with me.box(
+        style=me.Style(
+            display="flex",
+            flex_direction="column",
+            align_items="center",
+            height="100%",
+        )
+    ):
+        me.text("Generated Video")
+        me.box(style=me.Style(height=8))
+        with me.box(style=me.Style(height="100%")):
+            if state.is_loading:
+                me.progress_spinner()
+            elif state.result_video:
+                video_url = state.result_video.replace(
+                    "gs://",
+                    "https://storage.mtls.cloud.google.com/",
+                )
+                print(f"video_url: {video_url}")
+                me.video(src=video_url, style=me.Style(border_radius=6))
+                with me.box(
+                    style=me.Style(
+                        display="flex",
+                        flex_direction="row",
+                        gap=5,
+                        align_items="center",
+                        padding=me.Padding(top=10),
+                    )
+                ):
+                    me.text(state.timing)
+                    if not state.veo_model == "3.0":
+                        me.select(
+                            label="extend",
+                            options=[
+                                me.SelectOption(label="None", value="0"),
+                                me.SelectOption(label="4 seconds", value="4"),
+                                me.SelectOption(label="5 seconds", value="5"),
+                                me.SelectOption(label="6 seconds", value="6"),
+                                me.SelectOption(label="7 seconds", value="7"),
+                            ],
+                            appearance="outline",
+                            style=me.Style(),
+                            value=f"{state.video_extend_length}",
+                            on_selection_change=on_selection_change_extend_length,
+                        )
+                        me.button(
+                            label="Extend",
+                            on_click=on_click_extend,
+                            disabled=True if state.video_extend_length == 0 else False,
+                        )
+
+
+def on_selection_change_extend_length(e: me.SelectSelectionChangeEvent):
+    """Adjust the video extend length in seconds based on user event"""
+    state = me.state(PageState)
+    state.video_extend_length = int(e.value)
+
+
+def on_click_extend(e: me.ClickEvent):
+    """Extend video"""
+    state = me.state(PageState)
+    print(
+        f"You would like to extend {state.result_video} by {state.video_extend_length} seconds."
+    )
+    print(f"Continue the scene {state.veo_prompt_input} ...")
