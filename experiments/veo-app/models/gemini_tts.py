cfb9fbc456b23a8295de0c2e5de89439d6632cda
csantos <csantos@users.noreply.github.com>
2025-09-11 10:13:59 -0700

    fix(gemini-tts): switch to TTS client instead of REST call - Switch to use TextToSpeechClient instead of making REST POST call. Using REST call directly required additional perm serviceUsageConsumer. Switch to client avoid this and was fewer lines of code.

--- experiments/veo-app/models/gemini_tts.py
+++ experiments/veo-app/models/gemini_tts.py
@@ -13,16 +13,7 @@
 # limitations under the License.
 
 """Model for Gemini Text-to-Speech."""
-import base64
-
-import google.auth
-import google.auth.transport.requests
-import requests
-
-from config.default import Default
-
-cfg = Default()
-
+import google.cloud.texttospeech as texttospeech
 
 def synthesize_speech(text: str, prompt: str, model_name: str, voice_name: str, language_code: str) -> bytes:
     """
@@ -37,39 +28,10 @@ def synthesize_speech(text: str, prompt: str, model_name: str, voice_name: str, 
     Returns:
         The synthesized audio in bytes.
     """
-    creds, _ = google.auth.default(
-        scopes=["https://www.googleapis.com/auth/cloud-platform"]
-    )
-    auth_req = google.auth.transport.requests.Request()
-    creds.refresh(auth_req)
+    client = texttospeech.TextToSpeechClient()
+        
+    response = client.synthesize_speech(input=texttospeech.SynthesisInput(text=text, prompt=prompt),
+                                        voice=texttospeech.VoiceSelectionParams(language_code=language_code, name=voice_name, model_name=model_name),
+                                        audio_config=texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.LINEAR16))
 
-    url = "https://texttospeech.googleapis.com/v1/text:synthesize"
-    headers = {
-        "Authorization": f"Bearer {creds.token}",
-        "x-goog-user-project": cfg.PROJECT_ID,
-        "Content-Type": "application/json; charset=utf-8",
-    }
-    payload = {
-        "input": {
-            "text": text,
-            "prompt": prompt,
-        },
-        "voice": {
-            "languageCode": language_code,
-            "name": voice_name,
-            "model_name": model_name,
-        },
-        "audioConfig": {
-            "audioEncoding": "LINEAR16",
-        },
-    }
-
-    response = requests.post(url, headers=headers, json=payload)
-    if response.status_code != 200:
-        print(f"Error from TTS API: {response.status_code} - {response.text}")
-    response.raise_for_status()  # Raise an exception for bad status codes
-
-    audio_content_base64 = response.json()["audioContent"]
-    audio_bytes = base64.b64decode(audio_content_base64)
-
-    return audio_bytes
+    return response.audio_content
